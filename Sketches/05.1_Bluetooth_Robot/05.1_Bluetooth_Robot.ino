9392f2ff50a6b0e847c3eceb9fde1f97b58acc6a
qinzian2000 <2191847102@qq.com>
2024-11-14 10:51:20 +0800

    Update C tutorial
    Update Chapter 05.1 in the C tutorial to optimize Bluetooth configuration

--- Sketches/05.1_Bluetooth_Robot/05.1_Bluetooth_Robot.ino
+++ Sketches/05.1_Bluetooth_Robot/05.1_Bluetooth_Robot.ino
@@ -14,6 +14,8 @@ SerialCommand SCmd(BTserial);
 
 String inputString = "";    // a String to hold incoming data
 bool stringComplete = false;// whether the string is complete
+int baudRate[] = {2400, 9600, 19200, 115200};  
+int baudRateCount = sizeof(baudRate) / sizeof(baudRate[0]);  
 
 void receiveStop() {
   delay(10);
@@ -22,17 +24,24 @@ void receiveStop() {
 
 void setup() {
   Serial.begin(115200);
+  // Loop through each baud rate and send commands 
+  for(int i = 0; i < baudRateCount; i++){
+    BTserial.begin(baudRate[i]);
+    delay(200);
+    BTserial.println("AT+NAME=BT05");//Set the radio called Robot
+    delay(200);
+    BTserial.println("AT+ROLE=0");
+    delay(200);
+    BTserial.println("AT+UART=4");//1=2400 2=9600 3=19200 4=115200
+    delay(200);
+
+    //SoftwareSerial does not support dynamic adjustment of baud rate, so close the serial
+    BTserial.end();
+  }
+  
   BTserial.begin(115200);
   delay(2000);
-  BTserial.println("AT+UART?\r\n");   
-  delay(200);
-  BTserial.println("AT+UART=115200\r\n"); 
-  delay(200);
-  BTserial.println("AT+NAME=BT05\r\n");//Set the radio called Robot
-  delay(200);
-  // BTserial.println("“AT+ROLE=2\r\n”");     //Set Bluetooth to slave mode.
-  // BTserial.println("AT+RESET\r\n");     //Reset (restart)
-  
+
   SCmd.addDefaultHandler(receiveStop);
   inputString.reserve(200);
   Serial.println("Set the name of the Bluetooth module to BT05.");
